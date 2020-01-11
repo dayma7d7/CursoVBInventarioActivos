cb94df22fac32faa1148ca3c31a743b133e67f68
dayma7d7 <dayma7d7@gmail.com>
2020-01-11 23:57:23 +0100

    Agregar archivos de proyecto.

---
+++ ConexionBD/ConectarBD.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+
+Module ConectarBD
+    Public ip As String = "", user As String = "", pass As String = "", bd As String = ""
+    Public conexion As SqlConnection
+    Public adapter As SqlDataAdapter
+    Public tabla As DataTable
+
+    Public nombre As String
+    Public usuario As String
+    Public tipousuario As String
+
+    Public Sub establecerConexion()
+        Try
+            If File.Exists(Application.StartupPath + "IP.txt") Then
+                ip = My.Computer.FileSystem.ReadAllText(Application.StartupPath + "IP.txt")
+            End If
+            If File.Exists(Application.StartupPath + "USER.txt") Then
+                user = My.Computer.FileSystem.ReadAllText(Application.StartupPath + "USER.txt")
+            End If
+            If File.Exists(Application.StartupPath + "PASS.txt") Then
+                pass = My.Computer.FileSystem.ReadAllText(Application.StartupPath + "PASS.txt")
+            End If
+            If File.Exists(Application.StartupPath + "bd.txt") Then
+                bd = My.Computer.FileSystem.ReadAllText(Application.StartupPath + "bd.txt")
+            End If
+
+            If ip.Length = 0 Or user.Length = 0 Or pass.Length = 0 Or bd.Length = 0 Then
+                MsgBox("Faltan parametros de conexión", vbCritical, "ERROR DE CONEXIÓN")
+            Else
+                conexion = New SqlConnection("Data Source=" & ip.Trim & "; Initial Catalog=" & bd.Trim & "; User Id=" & user.Trim & "; Password=" & pass.Trim & "; ")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+
+    Public Function obtenerConexion() As String
+        Dim miconexion As String = ""
+        Try
+            If File.Exists(Application.StartupPath + "IP.txt") Then
+                ip = My.Computer.FileSystem.ReadAllText(Application.StartupPath + "IP.txt")
+            End If
+            If File.Exists(Application.StartupPath + "USER.txt") Then
+                user = My.Computer.FileSystem.ReadAllText(Application.StartupPath + "USER.txt")
+            End If
+            If File.Exists(Application.StartupPath + "PASS.txt") Then
+                pass = My.Computer.FileSystem.ReadAllText(Application.StartupPath + "PASS.txt")
+            End If
+            If File.Exists(Application.StartupPath + "bd.txt") Then
+                bd = My.Computer.FileSystem.ReadAllText(Application.StartupPath + "bd.txt")
+            End If
+
+            If ip.Length = 0 Or user.Length = 0 Or pass.Length = 0 Or bd.Length = 0 Then
+                MsgBox("Faltan parametros de conexión", vbCritical, "ERROR DE CONEXIÓN")
+            Else
+                miconexion = "Data Source=" & ip.Trim & "; Initial Catalog=" & bd.Trim & "; User Id=" & user.Trim & "; Password=" & pass.Trim & "; "
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical)
+        End Try
+        Return miconexion
+
+    End Function
+
+End Module
---
+++ Formularios/frmConfiguracion.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmConfiguracion
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(63, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "IP / Servidor"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(63, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Base de Datos"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(63, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Usuario"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(63, 136)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(69, 17)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Password"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(173, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(224, 22)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(173, 76)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(224, 22)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(173, 105)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(224, 22)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(173, 134)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(224, 22)
+        Me.TextBox4.TabIndex = 7
+        '
+        'btnConectar
+        '
+        Me.btnConectar.Location = New System.Drawing.Point(173, 181)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(75, 29)
+        Me.btnConectar.TabIndex = 8
+        Me.btnConectar.Text = "&Conectar"
+        Me.btnConectar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(322, 181)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(75, 29)
+        Me.btnCancelar.TabIndex = 9
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'frmConfiguracion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(460, 236)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmConfiguracion"
+        Me.Text = "Configuración del Servidor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents btnConectar As Button
+    Friend WithEvents btnCancelar As Button
+End Class
